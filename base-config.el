227e4294ecc709c10ba25bf179f22438ecd95557
Per Wiklander <per@wiklanderconsulting.se>
2012-09-01 13:07:34 +0200

    Add jade mode

--- base-config.el
+++ base-config.el
@@ -86,6 +86,11 @@
 
 (require 'scala-mode-auto)
 
+(require 'sws-mode)
+(require 'jade-mode)    
+(add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
+(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
 (iswitchb-mode 1)
 (setq iswitchb-buffer-ignore '("^\\*Messages"))
 
